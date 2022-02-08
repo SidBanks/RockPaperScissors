c7a38ce5e56e29b3e8aaf2975770d1192776ce56
Sid <sidbanks@gmail.com>
2022-02-08 02:38:14 -0600

    Added logic for playRound function.

--- script.js
+++ script.js
@@ -8,8 +8,22 @@ let computerSelection = function computerPlay(plays) {
 
 //Round of play function (playRound) playRound accepts 2 parameters [computerSelection, playerSelection] and declares a winner.  The logic is a 7 step if statement
 function playRound(playerSelection, computerSelection) {
-
-}
+    if (playerSelection === 'rock'  && computerSelection === 'scissors') {
+        alert('You win Rock smashes Scissors!');
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        alert('You lose Paper covers Rock!');
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        alert('You win Paper covers Rock!');
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        alert('You lose Scissors cut Paper!');
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert('You win Scissors cut paper!');
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        alert('You lose Rock smashes Scissors!');
+    } else (playerSelection === '' && computerSelection === '') {
+        alert('You picked the same as the computer!  It\'s a tie');
+    }
+} 
 
 //Game play function (Game) Opens a while loop that calls (playRound) dispays round results message. Increments [playerWins OR computerWins].  Loops 4 more times for a total of 5.  Compares [playerWins to computerWins] and dispays a message about the winner of the game.
 
