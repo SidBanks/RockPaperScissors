379a99fafe6116dfe1cde32e62c60f38e81e414b
Sid <sidbanks@gmail.com>
2022-02-12 00:55:01 -0600

    Refactoring and correcting poor layaut of code.

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-//Setup and assign variables to handle win counting[playerWins, computerWins, tie].
-let playerWins = 0;
-let computerWins = 0;
-let tie = 0;
-
 //Set up an array (plays)for the three possible play options [rock, paper, scissors]
-let plays = ["rock", "paper", "scissors"];
+let plays = [`rock`, `paper`, `scissors`];
+
+
+//Prompt functions (playerInput) player to enter [rock, paper, scissors]. Assigns the input to [playerSelection]
+//playerSelection.toLowerCase.  Compares player input to plays array [rock, paper, scissors] if input is not one of the three displays message asking for valid input and allows user to try again.
+function input() {
+    let playerInput = prompt('Please enter Rock, Paper, or Scissors');
+     playerInput.toLowerCase();
+     if (plays.indexOf(playerInput) !== -1) {
+         let playerSelection = playerInput;
+         return playerSelection;
+     } else { (alert(`You have to input Rock, Paper, or Scissors!`));
+    }
+}
 
 //Math function (computerPlay) that randomly selects an item from the array and assigns it to a variable [computerSelection]
-let computerSelection = function computerPlay(plays) {
-    return plays[Math.floor(Math.random()*plays.length)];
+function computerPlay(plays) {
+    let computerSelection = plays[Math.floor(Math.random()*plays.length)];
+    return computerSelection;
 }
 
 //Round of play function (playRound) playRound accepts 2 parameters [computerSelection, playerSelection] and declares a winner.  The logic is a 7 step if else statement.
@@ -32,14 +41,17 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         alert('You lose Rock smashes Scissors!');
         computerWins++
-    } else (alert('You picked the same as the computer!  It\'s a tie'));
+    } else { (alert('You picked the same as the computer!  It\'s a tie'));
         tie++
+    }
 } 
 
 //Game play function (game) Opens a while loop that calls (playRound) Loops 4 more times for a total of 5.  Compares [playerWins to computerWins] and dispays a message about the winner of the game.
 function game() {
     let n = 0;
     while (n < 5) {
+        input();
+        computerPlay(plays);
         playRound(playerSelection, computerSelection);
         n++
     }
@@ -47,11 +59,14 @@ function game() {
         alert(`You won ${playerWins} to ${computerWins}!`);
     } else if (playerWins < computerWins) {
         alert(`You lose ${playerWins} to ${computerWins}!`);
-    } else (alert(`You managed a tie!?`));
+    } else { (alert(`You managed a tie!?`));
+    }
 }
 
-//Prompt functions (playerInput) player to enter [rock, paper, scissors]. Assigns the input to [playerSelection]
-//playerSelection.toLowerCase.  Compares player input to [rock, paper, scissors] if input is not one of the three displays message asking for valid input and allows user to try again.
+//Setup and assign variables to handle win counting[playerWins, computerWins, tie].
+let playerWins;
+let computerWins;
+let tie;
 
 
 
